aa6393673ceee6e85a7e9cad01533ac0300e6c51
Danil <fomindanjj@rambler.ru>
2023-05-15 09:56:54 +0700

    added colors, table with interfaces, logging and saving packets to pcap file

--- PacketInspector.py
+++ PacketInspector.py
@@ -2,20 +2,23 @@ import socket
 import argparse
 import platform
 import ifaddr
-import json
-import struct
-
+from colorama import init, Fore, Style
+from prettytable import PrettyTable
+import datetime
+import logging
+import pcapfile
 
 def parse_args():
   '''
   Parse command line arguments
   '''
   parser = argparse.ArgumentParser(description='packet sniffer with python by d00m_r34p3r')
+  parser.add_argument('-I', '--interactive', action='store_true', help='interactive mode for settings')
   parser.add_argument('-p', '--protocol', metavar='protocol',  type=str, default='tcp', help='protocol to filter by (tcp, udp, icmp)')
   parser.add_argument('-i', '--interface', metavar='interface', type=str, help='interface to listen on')
   parser.add_argument('-r', '--raw', action='store_true', help='output packet contents in raw format')
   parser.add_argument('-hd', '--header', action='store_true', help='output header')
-  parser.add_argument('-I', '--interactive', action='store_true', help='interactive mode for settings')
+  parser.add_argument('-o', '--output', metavar='filename', type=str, help='Output file name')
 
   return parser.parse_args()
 
@@ -23,22 +26,36 @@ def get_interfaces():
   '''
   Get list of interfaces
   '''
-  list_of_interfaces = []
+  available_interfaces = []
   interfaces = ifaddr.get_adapters()
 
-  for interface in interfaces:
-    interface_for_list = {}
-    interface_for_list['name'] = interface.nice_name
-    interface_for_list['ip'] = interface.ips[1].ip
-    interface_for_list['mac'] = interface.ips[0].ip[0]
-    list_of_interfaces.append(interface_for_list)
+  for adapter in interfaces:
+    interface = {}
+    interface['name'] = adapter.nice_name
+    interface['ip'] = adapter.ips[1].ip
+    interface['mac'] = adapter.ips[0].ip[0]
+    available_interfaces.append(interface)
 
-  return list_of_interfaces
+  return available_interfaces
 
-interfaces = get_interfaces()
+def print_table_with_interfaces(interfaces):
+  table = PrettyTable()
+  table.field_names = ["#", "Interface", "Ip"]
+  for i, interface in enumerate(interfaces):
+    table.add_row([i+1, interface['name'], interface['ip']])
+  print(table)
+
+def print_color(text, color=None):
+  if color is not None:
+    color_obj = getattr(Fore, color.upper(), None)
+    if color_obj is not None:
+      print(color_obj + text + Style.RESET_ALL)
+      return
+  print(text)  
+  
 
 def print_logo():
-  print(
+  print(Fore.GREEN + 
           ' _____           _        _   _____                           _                  \n'
           '|  __ \         | |      | | |_   _|                         | |                 \n'
           '| |__) |_ _  ___| | _____| |_  | |  _ __  ___ _ __   ___  ___| |_ ___  _ __      \n'
@@ -47,15 +64,16 @@ def print_logo():
           '|_|   \__,_|\___|_|\_\___|\__|_____|_| |_|___/ .__/ \___|\___|\__\___/|_|        \n'
           '                                             | |                                 \n'
           '                                             |_|                                 \n'
-        )
+        + Style.RESET_ALL)
   
-def choose_interface():
+def choose_interface(interfaces):
   '''
   Select the interface to sniff on
   '''
-  print('Available interfaces, choose one of them:')
-  for i, interface in enumerate(interfaces):
-    print(f"{i+1}: {interface['name']}, {interface['ip']}")
+  print(f'[*] Available interfaces, enter a number between 1 and {len(interfaces)}:')
+  # for i, interface in enumerate(interfaces):
+  #   print(f"{i+1}: {interface['name']}, {interface['ip']}")
+  print_table_with_interfaces(interfaces)
   while True:
     try:
         choice = int(input('\nEnter number of interface: '))
@@ -63,85 +81,114 @@ def choose_interface():
             raise ValueError
         break
     except KeyboardInterrupt:
-      print("\nExiting...")
+      print('\nExiting...')
       exit(0)
     except ValueError:
-      print("Incorrect number, try again!")
+      print_color('[Err] Incorrect number, try again!', 'yellow')
         
   return interfaces[choice-1]
+
+def get_sniffer_socket(system, interface):
+  '''
+  Returns a socket for sniffing
+  '''
+  if system == 'Windows':
+    sniffer_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+    sniffer_socket.bind((interface['ip'], 0))
+    sniffer_socket.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+    sniffer_socket.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON) 
+        
+  elif system == 'Linux':
+    sniffer_socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+    sniffer_socket.bind((interface['name'], 0))
+
+  return sniffer_socket
   
 def main(): 
+  init()
   print_logo()
     
   args = parse_args()  
   system = platform.system()
+  interfaces = get_interfaces()
   
   if not args.interactive and not args.interface:
-    print('You must specify an interface or use interactive mode!')
+    print_color('[Err] You must specify an interface or use interactive mode!', 'red')
     exit(0)
   
   if args.interactive:
-    interface = choose_interface()
-    print(f"Your choice: {interface['name']}\n")
+    interface = choose_interface(interfaces)
+    print(f"[*] Your choice: {interface['name']}\n")
   
   else:
     interface = next((x for x in interfaces if x['name'] == args.interface), None)
     if interface is None:
-      print(f"Interface {args.interface} not found")
+      print_color(f'[Err] Interface {args.interface} not found', 'red')
       exit(0) 
+      
   
   if not args.protocol and not args.interactive:
-    print('You must specify the protocol or use interactive mode!')
+    print_color('[Err] You must specify the protocol or use interactive mode!', 'red')
     exit(0)
     
   if args.interactive:
     while True:
-      protocol = input("Enter protocol to sniff for (TCP, UDP, ICMP): ")
+      protocol = input('Enter protocol to sniff for (TCP, UDP, ICMP): ')
       if protocol.upper() in ['TCP', 'UDP', 'ICMP']:
         protocol = protocol.upper()
         break
       else:
-        print('Invalid protocol! Try again\n')
+        print_color('[Err] Invalid protocol! Try again\n', 'yellow')
   else:
     if args.protocol.upper() in ['TCP', 'UDP', 'ICMP']:
       protocol = args.protocol.upper()
     else:
-      print(f'Incorrect protocol! {args.protocol} is not TCP, UDP or ICMP')
+      print_color(f'[Err] Incorrect protocol! {args.protocol} is not TCP, UDP or ICMP','yellow')
       exit(0)  
 
-  
-  
   try:
-    print(interface)
-    if system == "Windows":
-        sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
-        sock.bind((interface['ip'], 0))
-        sock.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-        sock.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-        
-    elif system == "Linux":
-        sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
-        sock.bind((interface['ip'], 0))
+    sniffer_socket = get_sniffer_socket(system, interface)
   except KeyboardInterrupt:
-    print("\nExiting...")
+    print('\nExiting...')
     exit(0)
   except Exception as ex:
-    print(f'\n[*] {ex}\nTry again with another interface!\n')
+    print_color(f'\n[Err] {ex}\nTry again with another interface!\n', 'yellow')
     if args.interactive:
-      choose_interface()
+      interface = choose_interface(interfaces)
     else:
       exit(1)
-
+      
+  output_file = args.output
+  if output_file:
+    try:
+      pcap_file = open(args.output, 'wb')
+      logging.info(f"Output file: {args.output}")
+    except IOError:
+      print_color(f'[Err] Unable to open file {args.output}', 'red')
+      exit(0)
+      
+  print_color(f"\n[*] Sniffing started on interface {interface['name']}\n", 'green')    
+  start_time = datetime.datetime.now()
   while True:
     try:
-      print(sock.recvfrom(65535))
+      print(sniffer_socket.recvfrom(65535))
     except KeyboardInterrupt:
-      print("\nExiting...")
-      if system == "Windows":
-        sock.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+      print('\nExiting...')
+      end_time = datetime.datetime.now()
+      print("Total sniffing time:", end_time - start_time) # перенести вывод, добавить статистику по пакетам
+      if system == 'Windows':
+        sniffer_socket.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
       exit(0)
+    except socket.error as e:
+      print_color(f"[Err] Error: {e}", 'red')
+      logging.error(f"[Err] Error: {e}")
+    finally:
+      sniffer_socket.close()
+      if args.output:
+        pcap_file.close()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+  logging.basicConfig(level=logging.ERROR, filename="sniffer.log",filemode="a")
   main()
 
 
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PacketInspector
-## Packet sniffing in python
+### Packet sniffing in python
+
+### Installation
+To install dependencies for this program you can use scripts:  
+ • for unix systems - install_dependencies.sh  
+ • for windows - install_dependencies.bat  
+
+### About
+You can start PacketInspector with -h argument to see all opportunities
+
