5eb3ff61bb884fe4e178055cd08cdbc5125e71a8
EvtDanya <fomindanjj@rambler.ru>
2023-12-12 08:43:47 +0700

    Some fixes

--- install/install.bat
+++ install/install.bat
@@ -1,15 +1,3 @@
-<<<<<<< HEAD:install/install.bat
-::Windows only
-@echo off
-python --version 3>NUL
-if %errorlevel% equ 0 (
-    echo Python is installed! Continue...
-    pip install -r requirements.txt
-    echo Dependencies installed successfully! Now you can run tcp-proxy.py and close this window!
-) else (
-    echo Python is not installed. Please download and install Python from https://www.python.org/downloads/
-)
-=======
 ::Windows only
 @echo off
 python --version 3>NUL
@@ -20,5 +8,4 @@ if %errorlevel% equ 0 (
 ) else (
     echo Python is not installed. Please download and install Python from https://www.python.org/downloads/
 )
->>>>>>> 1816e615cdd3890e6bc432071a30d5bb06757eae:install/install_dependencies.bat
 cmd /k
